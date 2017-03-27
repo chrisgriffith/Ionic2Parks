04aa52091070bfe76b9c921d2f00bfc91d2a1fc5
Chris Griffith <chris.griffith@gmail.com>
2017-03-26 17:08:52 -0700

    Updating Ionic Native 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 import { ParkListPage } from '../pages/park-list/park-list';
 import { ParkMapPage } from '../pages/park-map/park-map';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -25,6 +27,9 @@ import { ParkDetailsPage } from '../pages/park-details/park-details';
     TabsPage,
     ParkDetailsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
