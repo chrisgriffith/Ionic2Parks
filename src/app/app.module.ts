236ce4bf0c81802f0caa3b00a978f0814dcf117e
Chris Griffith <chris.griffith@gmail.com>
2017-04-16 10:58:55 -0700

    Updating for Ionic 3 support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ParkListPage } from '../pages/park-list/park-list';
-import { ParkMapPage} from '../pages/park-map/park-map';
+import { ParkMapPage } from '../pages/park-map/park-map';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
@@ -15,6 +17,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
+    BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -27,6 +31,6 @@ import { TabsPage } from '../pages/tabs/tabs';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}]
+    { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
